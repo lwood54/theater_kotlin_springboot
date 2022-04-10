20daef9a000b8f6d566d66632d4656fd57e04ba6
lwood54 <lwood3499@gmail.com>
2022-04-10 15:31:35 -0500

    first commit

---
+++ src/main/kotlin/com/lwood54/theater/TheaterApplication.kt
@@ -0,0 +1,11 @@
+package com.lwood54.theater
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheaterApplication
+
+fun main(args: Array<String>) {
+	runApplication<TheaterApplication>(*args)
+}
